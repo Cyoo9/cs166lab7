65d0b965c0abde8eaecf1ed5adb651ececf15cd7
Taeho Yoo <tyoo002@wch133-37.cs.ucr.edu>
2021-02-26 11:35:01 -0800

    finished 2nd query

--- queries.sql
+++ queries.sql
@@ -2,4 +2,6 @@ SELECT COUNT (*)
 FROM part_nyc
 WHERE on_hand > 70;
 
-
+SELECT COUNT(part_sfo.on_hand)
+FROM part_sfo, part_nyc, color
+WHERE part_sfo.on_hand = part_nyc.on_hand AND part_sfo.color = part_nyc.color AND part_sfo.color = color.color_id AND color_name = 'Red';
