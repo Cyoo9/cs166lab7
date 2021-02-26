2b3424b5eae7a8e6c7a080a44c637738e802df91
Taeho Yoo <tyoo002@wch133-37.cs.ucr.edu>
2021-02-26 12:58:01 -0800

    finished lab

--- create_indexes.sql
+++ create_indexes.sql
@@ -1,9 +1,51 @@
-CREATE INDEX part_nyc_number_index
+CREATE INDEX part_nyc_on_hand
 ON part_nyc
-[USING BTREE]
-(part_number)
+USING BTREE
+(on_hand);
 
-CREATE INDEX part_sfo_number_index
+CREATE INDEX part_sfo_on_hand
 on part_sfo
-[USING BTREE]
-(part_number)
+USING BTREE
+(on_hand);
+
+CREATE INDEX part_nyc_color
+on part_nyc
+USING BTREE
+(color);
+
+CREATE INDEX part_sfo_color
+on part_sfo
+USING BTREE
+(color);
+
+CREATE INDEX part_nyc_supplier
+on part_nyc
+USING BTREE
+(supplier);
+
+CREATE INDEX part_sfo_supplier
+on part_sfo
+USING BTREE
+(supplier);
+
+CREATE INDEX color_color_id
+on color
+USING BTREE
+(color_id);
+
+CREATE INDEX supplier_supplier_id
+on supplier
+USING BTREE
+(supplier_id);
+
+CREATE INDEX supplier_supplier_name
+on supplier
+USING BTREE
+(supplier_name);
+
+CREATE INDEX color_color_name
+on color
+USING BTREE 
+(color_name);
+
+
